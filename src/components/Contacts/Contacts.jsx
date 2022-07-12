5952d7653dd3e7f20856a9b22ff713e76b95b32d
Mykhailo Kuzich <timmkedth@gmail.com>
2022-07-12 08:57:52 +0300

    changed render condition

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -16,19 +16,21 @@ export const Contacts = ({ contacts, filterChange, filter, deleteContact }) => {
       </label>
       <ul className={styles.list}>
         {contacts.map(contact => {
-          return contact.name.toLowerCase().includes(filter.toLowerCase()) ? (
-            <li className={styles.contact} key={contact.id}>
-              {contact.name}: {contact.number}
-              <button
-                className={styles.btn}
-                type="button"
-                onClick={deleteContact}
-                id={contact.id}
-              >
-                Delete
-              </button>
-            </li>
-          ) : null;
+          return (
+            contact.name.toLowerCase().includes(filter.toLowerCase()) && (
+              <li className={styles.contact} key={contact.id}>
+                {contact.name}: {contact.number}
+                <button
+                  className={styles.btn}
+                  type="button"
+                  onClick={deleteContact}
+                  id={contact.id}
+                >
+                  Delete
+                </button>
+              </li>
+            )
+          );
         })}
       </ul>
     </>
